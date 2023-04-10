57d85dc048e3cfb3ce493954bd550f58dec0126b
Tiago de Oliveira <tiagodeoliveiravillalva@gmail.com>
2023-04-10 19:41:08 -0300

    implemetacao das operacoes matimaticas: soma, subtracao, divisao, multiplicacao, raiz quadrada.

--- src/main/java/br/com/tiago/MatchController.java
+++ src/main/java/br/com/tiago/MatchController.java
@@ -20,6 +20,8 @@ public class MatchController {
 	 * o REQUESTMAPPING tem que ser igual ao definido aqui no EndPoint*/
 	/*, method=RequestMethod.GET não é obrigatório porém é uma boa pratica
 	 * quando eu não expecifico pow default ele ja é GET*/
+	
+	/*------------------METODO DE SOMA---------------*/
 	@RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method=RequestMethod.GET)
 	public Double sum (
 			@PathVariable(value = "numberOne") String numberOne, /*variavel da URL*/
@@ -37,6 +39,7 @@ public class MatchController {
 		return convertToDouble(numberOne) + convertToDouble(numberTwo);
 	}
 	
+	/*------------------METODO DE SUBSTRAÇÃO---------------*/
 	@RequestMapping(value = "/subtraction/{numberOne}/{numberTwo}", method=RequestMethod.GET)
 	public Double subtraction(
 			@PathVariable(value = "numberOne") String numberOne, /*variavel da URL*/
@@ -54,6 +57,80 @@ public class MatchController {
 		return convertToDouble(numberOne) - convertToDouble(numberTwo);
 	}
 
+	
+	/*------------------METODO DE MULTIPLICAÇÃO---------------*/
+	@RequestMapping(value = "/multiplication/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+	public Double multiplication(
+			@PathVariable(value = "numberOne") String numberOne, /*variavel da URL*/
+			@PathVariable(value = "numberTwo") String numberTwo /*variavel da URL*/
+
+			) throws Exception{
+		
+		/*depois de receber as informações que o usuario passou vamos verificar
+		 * se é numerico*/
+		if (!isNumeric(numberOne) || !isNumeric (numberTwo)) {
+			throw new UnsupportedMathOperationException("Please set a numeric value!");
+		}
+		
+		/*se for numérico, vou fazer a conversão para DOUBLE e realizar a SOMA*/
+		return convertToDouble(numberOne) * convertToDouble(numberTwo);
+	}
+	
+	
+	/*------------------METODO DE DIVISÃO---------------*/
+	@RequestMapping(value = "/division/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+	public Double division(
+			@PathVariable(value = "numberOne") String numberOne, /*variavel da URL*/
+			@PathVariable(value = "numberTwo") String numberTwo /*variavel da URL*/
+
+			) throws Exception{
+		
+		/*depois de receber as informações que o usuario passou vamos verificar
+		 * se é numerico*/
+		if (!isNumeric(numberOne) || !isNumeric (numberTwo)) {
+			throw new UnsupportedMathOperationException("Please set a numeric value!");
+		}
+		
+		/*se for numérico, vou fazer a conversão para DOUBLE e realizar a SOMA*/
+		return convertToDouble(numberOne) / convertToDouble(numberTwo);
+	}
+	
+	/*------------------METODO DE MÉDIA---------------*/
+	@RequestMapping(value = "/mean/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+	public Double mean (
+			@PathVariable(value = "numberOne") String numberOne, /*variavel da URL*/
+			@PathVariable(value = "numberTwo") String numberTwo /*variavel da URL*/
+
+			) throws Exception{
+		
+		/*depois de receber as informações que o usuario passou vamos verificar
+		 * se é numerico*/
+		if (!isNumeric(numberOne) || !isNumeric (numberTwo)) {
+			throw new UnsupportedMathOperationException("Please set a numeric value!");
+		}
+		
+		/*se for numérico, vou fazer a conversão para DOUBLE e realizar a SOMA*/
+		return convertToDouble(numberOne) + convertToDouble(numberTwo)/2;
+	}
+	
+	/*------------------METODO DE RAIZ QUADRADA---------------*/
+	@RequestMapping(value = "/squareRoot/{number}", method=RequestMethod.GET)
+	public Double squareRoot (
+			@PathVariable(value = "number") String number
+
+			) throws Exception{
+		
+		/*depois de receber as informações que o usuario passou vamos verificar
+		 * se é numerico*/
+		if (!isNumeric(number)) {
+			throw new UnsupportedMathOperationException("Please set a numeric value!");
+		}
+		
+		/*se for numérico, vou fazer a conversão para DOUBLE e realizar a SOMA*/
+		return Math.sqrt( convertToDouble(number));
+	}
+	
+	
 	private boolean isNumeric(String strNumber) {
 		if(strNumber == null) return false;/*verifica se é null, poderia retornar uma exceção*/
 		String number = strNumber.replaceAll(",", ".");/*condiciona o usuario a poder usar , ou . nos numeros*/
