40feb09df3410f821d146d0e534b60b386e381a7
Tiago de Oliveira <tiagodeoliveiravillalva@gmail.com>
2023-04-13 21:09:47 -0300

    Implementacao da classe model Person, classe PersonServices e Person Controller, foi implementado o endPoint FindById e FindAll para Person.

---
+++ src/main/java/br/com/tiago/controllers/PersonController.java
@@ -0,0 +1,38 @@
+package br.com.tiago.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.tiago.model.Person;
+import br.com.tiago.services.PersonServices;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+	@Autowired
+	private PersonServices personServices;
+
+	/*------------------METODO FINDBYID---------------*/
+	@RequestMapping(value = "/{id}", 
+			method = RequestMethod.GET,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person findById(@PathVariable(value = "id") String id) throws Exception {
+
+		return personServices.findById(id);
+	}
+	
+	@RequestMapping 
+			( method = RequestMethod.GET,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Person> findAll(){
+		return personServices.findAll();
+	}
+
+}
