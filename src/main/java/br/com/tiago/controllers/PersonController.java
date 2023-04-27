2c47e2e9eaaecd7a4581ff2ee46e73181cbe4709
Tiago de Oliveira <tiagodeoliveiravillalva@gmail.com>
2023-04-27 19:40:16 -0300

    Refatorando classe PersonController utilizando as anotações expecificas de cada endPoint

--- src/main/java/br/com/tiago/controllers/PersonController.java
+++ src/main/java/br/com/tiago/controllers/PersonController.java
@@ -5,11 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.tiago.model.Person;
@@ -22,36 +24,8 @@ public class PersonController {
 	@Autowired
 	private PersonServices personServices;
 
-	/*----------CREATE--------*/
-	@RequestMapping ( method = RequestMethod.POST,
-			consumes = MediaType.APPLICATION_JSON_VALUE, /*cosome JSON*/
-			produces = MediaType.APPLICATION_JSON_VALUE) /*Produz JSON*/
-	public Person create(@RequestBody Person person) {
-		
-		return personServices.create(person);
-	}
-	
-	/*----------UPDATE--------*/
-	@RequestMapping ( method = RequestMethod.PUT,
-			consumes = MediaType.APPLICATION_JSON_VALUE, /*cosome JSON*/
-			produces = MediaType.APPLICATION_JSON_VALUE) /*Produz JSON*/
-	public Person update (@RequestBody Person person) {
-		return personServices.update(person);
-	}
-	
-	
-	
-	/*-----------DELETE-----------*/
-	@RequestMapping(value = "/{id}"
-			, method = RequestMethod.DELETE)
-	public void delete(@PathVariable(value = "id")Long id){
-	personServices.delete(id);
-	}
-	
-	
 	/*------------------METODO FINDBYID---------------*/
-	@RequestMapping(value = "/{id}", 
-			method = RequestMethod.GET,
+	@GetMapping(value = "/{id}", 
 			produces = MediaType.APPLICATION_JSON_VALUE)
 	public Person findById(@PathVariable(value = "id") Long id) throws Exception {
 
@@ -59,11 +33,35 @@ public class PersonController {
 	}
 	
 	/*---------------METODO FINDALL--------------*/
-	@RequestMapping 
-			( method = RequestMethod.GET,
-			produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Person> findAll(){
 		return personServices.findAll();
 	}
 
+		
+	/*----------CREATE--------*/
+	@PostMapping( consumes = MediaType.APPLICATION_JSON_VALUE, /*cosome JSON*/
+			produces = MediaType.APPLICATION_JSON_VALUE) /*Produz JSON*/
+	public Person create(@RequestBody Person person) {
+		
+		return personServices.create(person);
+	}
+	
+	/*----------UPDATE--------*/
+	@PutMapping ( consumes = MediaType.APPLICATION_JSON_VALUE, /*cosome JSON*/
+			produces = MediaType.APPLICATION_JSON_VALUE) /*Produz JSON*/
+	public Person update (@RequestBody Person person) {
+		return personServices.update(person);
+	}
+
+	
+	/*-----------DELETE-----------*/
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<?> delete(@PathVariable(value = "id")Long id){
+		personServices.delete(id);
+		return ResponseEntity.noContent().build();
+		/*noContent irá retornar statuscod 204*/
+	}
+	
+	
 }
